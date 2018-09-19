3804dd9920c16ba0b27aa855b474847fb317e89a
RekGRpth <rekgrpth@gmail.com>
2018-09-19 16:12:19 +0500

    Revert "Fix build with nginx 1.11.6"
    This reverts commit 3b0f8e50abe69a0e800bdf87f51f144f5bbf9056.

--- src/ngx_postgres_module.c
+++ src/ngx_postgres_module.c
@@ -1310,7 +1310,6 @@ ngx_postgres_find_upstream(ngx_http_request_t *r, ngx_url_t *url)
             continue;
         }
 
-  #if (nginx_version < 1011006)
         if (uscfp[i]->default_port && url->default_port
             && (uscfp[i]->default_port != url->default_port))
         {
@@ -1318,7 +1317,6 @@ ngx_postgres_find_upstream(ngx_http_request_t *r, ngx_url_t *url)
             continue;
         }
 
-  #endif
         dd("returning");
         return uscfp[i];
     }
