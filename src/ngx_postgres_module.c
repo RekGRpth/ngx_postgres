73fdbbfe3a98670d0dc6bb98b1954474800f1ba3
RekGRpth <rekgrpth@gmail.com>
2019-09-13 12:45:31 +0500

    fix

--- src/ngx_postgres_module.c
+++ src/ngx_postgres_module.c
@@ -699,20 +699,19 @@ static char *ngx_postgres_conf_query(ngx_conf_t *cf, ngx_command_t *cmd, void *c
     u_char *p = query->sql.data, *s = sql.data, *e = sql.data + sql.len;
     for (ngx_uint_t k = 0; s < e; ) {
         if ((*p++ = *s++) == '$') {
-            p += ngx_sprintf(p, "%d", ++k) - p;
             ngx_str_t name;
-            for (name.data = s, name.len = 0; s++ < e && is_variable_character(*s); name.len++);
+            for (name.data = s, name.len = 0; s < e && is_variable_character(*s); s++, name.len++);
             if (!name.len) continue;
-            name.len++;
 //            ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "name = %V", &name);
             ngx_str_t oid = {0, NULL};
-            if (s[1] == ':' && s[2] == ':') for (s += 2, oid.data = s, oid.len = 0; s < e && is_variable_character(*s); s++, oid.len++);
-            if (!oid.len) { ngx_str_set(&oid, "TEXT"); }
+            if (*s == ':' && *(s+1) == ':') for (s += 2, oid.data = s, oid.len = 0; s < e && is_variable_character(*s); s++, oid.len++);
+            if (!oid.len) { p = ngx_copy(p, name.data, name.len); continue; }
             ngx_postgres_arg_t *arg;
             if (!(arg = ngx_array_push(&query->args))) { ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "%s:%d", __FILE__, __LINE__); return NGX_CONF_ERROR; }
             if ((ngx_int_t)(arg->index = ngx_http_get_variable_index(cf, &name)) == NGX_ERROR) { ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "postgres: invalid name \"%V\" in \"%V\" directive", &name, &cmd->name); return NGX_CONF_ERROR; }
             if (!(arg->oid = str2oid(&oid))) { ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "postgres: invalid oid \"%V\" in \"%V\" directive", &oid, &cmd->name);  return NGX_CONF_ERROR; }
 //            ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "oid = %V, oid = %d", &oid, arg->oid);
+            p += ngx_sprintf(p, "%d", ++k) - p;
         }
     }
     query->sql.len = p - query->sql.data;
