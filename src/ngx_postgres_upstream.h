7985ffc8aca1211a9cc41b9c71ca278aa0e483f0
RekGRpth <rekgrpth@gmail.com>
2020-03-10 11:51:24 +0500

    up

--- src/ngx_postgres_upstream.h
+++ src/ngx_postgres_upstream.h
@@ -34,7 +34,6 @@
 
 #include "ngx_postgres_module.h"
 
-
 typedef enum {
     state_db_connect,
     state_db_send_prepare,
@@ -108,13 +107,11 @@ typedef struct {
     ngx_queue_t                        queue;
 } ngx_postgres_save_t;
 
-
 char *ngx_postgres_query_conf(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 ngx_flag_t ngx_postgres_is_my_peer(const ngx_peer_connection_t *);
 ngx_int_t ngx_http_push_stream_add_msg_to_channel_my(ngx_log_t *log, ngx_str_t *id, ngx_str_t *text, ngx_str_t *event_id, ngx_str_t *event_type, ngx_flag_t store_messages, ngx_pool_t *temp_pool);
 ngx_int_t ngx_http_push_stream_delete_channel_my(ngx_log_t *log, ngx_str_t *id, u_char *text, size_t len, ngx_pool_t *temp_pool);
 ngx_int_t ngx_postgres_peer_init(ngx_http_request_t *r, ngx_http_upstream_srv_conf_t *upstream_srv_conf);
 void ngx_postgres_free_connection(ngx_postgres_common_t *, ngx_postgres_common_t *, ngx_flag_t);
-
 
 #endif /* _NGX_HTTP_UPSTREAM_POSTGRES_H_ */
