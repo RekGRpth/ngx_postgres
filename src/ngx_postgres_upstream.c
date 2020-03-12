315768d3b4bacf936e8389a317baaa08df9156ed
RekGRpth <rekgrpth@gmail.com>
2020-03-12 16:55:14 +0500

    up

--- src/ngx_postgres_upstream.c
+++ src/ngx_postgres_upstream.c
@@ -362,7 +362,7 @@ void ngx_postgres_free_connection(ngx_postgres_common_t *common, ngx_postgres_co
         if (common->connection->write->posted) { ngx_delete_posted_event(common->connection->write); }
         common->connection->read->closed = 1;
         common->connection->write->closed = 1;
-        if (common->connection->pool) {
+        if (common->connection->pool && !common->connection->close) {
             ngx_destroy_pool(common->connection->pool);
             common->connection->pool = NULL;
         }
