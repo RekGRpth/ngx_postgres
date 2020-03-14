71037485cfc486930ec2f0d61c43f058ed9c9b68
RekGRpth <rekgrpth@gmail.com>
2020-03-14 16:16:00 +0500

    u[

--- src/ngx_postgres_output.h
+++ src/ngx_postgres_output.h
@@ -4,6 +4,6 @@
 #include <ngx_http.h>
 
 char *ngx_postgres_output_conf(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
-ngx_int_t ngx_postgres_output_chain(ngx_http_request_t *);
+ngx_int_t ngx_postgres_output_chain(ngx_http_request_t *r);
 
 #endif /* _NGX_POSTGRES_OUTPUT_H_ */
