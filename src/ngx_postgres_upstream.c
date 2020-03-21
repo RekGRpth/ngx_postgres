c925ebd3a6ad46491d7db685259bab3dc436dfc8
RekGRpth <rekgrpth@gmail.com>
2020-03-21 17:50:54 +0500

    up

--- src/ngx_postgres_upstream.c
+++ src/ngx_postgres_upstream.c
@@ -23,13 +23,14 @@ static void ngx_postgres_save_to_free(ngx_postgres_data_t *pd, ngx_postgres_save
     c->log = pc->log;
     if (c->pool) c->pool->log = pc->log;
     c->read->log = pc->log;
+    if (c->read->timer_set) ngx_del_timer(c->read);
     c->sent = 0;
     c->write->log = pc->log;
+    if (c->write->timer_set) ngx_del_timer(c->write);
     pc->cached = 1;
     pc->name = &pdc->name;
     pc->sockaddr = pdc->sockaddr;
     pc->socklen = pdc->socklen;
-    if (psc->timeout.timer_set) ngx_del_timer(&psc->timeout);
 }
 
 
@@ -176,6 +177,7 @@ static void ngx_postgres_write_handler(ngx_event_t *ev) {
     ngx_postgres_save_t *ps = c->data;
     ngx_postgres_common_t *psc = &ps->common;
     if (c->close) { ngx_log_debug0(NGX_LOG_DEBUG_HTTP, ev->log, 0, "close"); goto close; }
+    if (c->write->timedout) { ngx_log_debug0(NGX_LOG_DEBUG_HTTP, ev->log, 0, "timedout"); goto close; }
     return;
 close:
     ngx_postgres_free_connection(psc);
@@ -245,6 +247,7 @@ static void ngx_postgres_read_handler(ngx_event_t *ev) {
     ngx_postgres_save_t *ps = c->data;
     ngx_postgres_common_t *psc = &ps->common;
     if (c->close) { ngx_log_debug0(NGX_LOG_DEBUG_HTTP, ev->log, 0, "close"); goto close; }
+    if (c->read->timedout) { ngx_log_debug0(NGX_LOG_DEBUG_HTTP, ev->log, 0, "timedout"); goto close; }
     if (!PQconsumeInput(psc->conn)) { ngx_log_error(NGX_LOG_ERR, ev->log, 0, "!PQconsumeInput and %s", PQerrorMessageMy(psc->conn)); goto close; }
     if (PQisBusy(psc->conn)) { ngx_log_debug0(NGX_LOG_DEBUG_HTTP, ev->log, 0, "PQisBusy"); return; }
     for (PGresult *res; (res = PQgetResult(psc->conn)); PQclear(res)) switch(PQresultStatus(res)) {
@@ -307,18 +310,6 @@ cont:
 }
 
 
-static void ngx_postgres_save_timeout(ngx_event_t *ev) {
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, ev->log, 0, "%s", __func__);
-    ngx_connection_t *c = ev->data;
-    ngx_postgres_save_t *ps = c->data;
-    ngx_postgres_common_t *psc = &ps->common;
-    ngx_postgres_free_connection(psc);
-    ngx_queue_remove(&ps->queue);
-    ngx_postgres_server_t *server = psc->server;
-    ngx_queue_insert_tail(&server->free.queue, &ps->queue);
-}
-
-
 static void ngx_postgres_free_to_save(ngx_postgres_data_t *pd, ngx_postgres_save_t *ps) {
     ngx_http_request_t *r = pd->request;
     ngx_postgres_common_t *pdc = &pd->common;
@@ -339,13 +330,11 @@ static void ngx_postgres_free_to_save(ngx_postgres_data_t *pd, ngx_postgres_save
     c->read->delayed = 0;
     c->read->handler = ngx_postgres_read_handler;
     c->read->log = c->log;
+    ngx_add_timer(c->read, server->ps.timeout);
     c->write->delayed = 0;
     c->write->handler = ngx_postgres_write_handler;
     c->write->log = c->log;
-    psc->timeout.log = c->log;
-    psc->timeout.data = c;
-    psc->timeout.handler = ngx_postgres_save_timeout;
-    ngx_add_timer(&psc->timeout, server->ps.timeout);
+    ngx_add_timer(c->write, server->ps.timeout);
 }
 
 
