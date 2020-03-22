0d55c6c7098cbba13ef58557909767f71e6fc5ac
RekGRpth <rekgrpth@gmail.com>
2020-03-22 17:04:23 +0500

    up

--- src/ngx_postgres_handler.c
+++ src/ngx_postgres_handler.c
@@ -5,23 +5,17 @@
 #include "ngx_postgres_upstream.h"
 
 
-static void ngx_postgres_write_event_handler(ngx_http_request_t *r, ngx_http_upstream_t *u) {
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "%s", __func__);
-    ngx_peer_connection_t *pc = &u->peer;
-    ngx_connection_t *c = pc->connection;
+static void ngx_postgres_event_handler(ngx_event_t *ev) {
+    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, ev->log, 0, "%s", __func__);
+    ngx_connection_t *c = ev->data;
+    ngx_postgres_data_t *pd = c->data;
+    ngx_http_request_t *r = pd->request;
+    ngx_http_upstream_t *u = r->upstream;
+    if (c->read->timedout) return ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_TIMEOUT);
     if (c->write->timedout) return ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_TIMEOUT);
     if (ngx_http_upstream_test_connect(c) != NGX_OK) return ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_ERROR);
     ngx_postgres_process_events(r);
-}
-
-
-static void ngx_postgres_read_event_handler(ngx_http_request_t *r, ngx_http_upstream_t *u) {
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "%s", __func__);
-    ngx_peer_connection_t *pc = &u->peer;
-    ngx_connection_t *c = pc->connection;
-    if (c->read->timedout) return ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_TIMEOUT);
-    if (ngx_http_upstream_test_connect(c) != NGX_OK) return ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_ERROR);
-    ngx_postgres_process_events(r);
+//    ngx_http_run_posted_requests(c);
 }
 
 
@@ -51,12 +45,13 @@ static ngx_int_t ngx_postgres_create_request(ngx_http_request_t *r) {
 static ngx_int_t ngx_postgres_reinit_request(ngx_http_request_t *r) {
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "%s", __func__);
     ngx_http_upstream_t *u = r->upstream;
-    u->read_event_handler = ngx_postgres_read_event_handler;
-    u->write_event_handler = ngx_postgres_write_event_handler;
     ngx_postgres_data_t *pd = u->peer.data;
     ngx_postgres_common_t *pdc = &pd->common;
+    ngx_connection_t *c = pdc->connection;
+    c->data = pd;
+    c->read->handler = ngx_postgres_event_handler;
+    c->write->handler = ngx_postgres_event_handler;
     if (pdc->state != state_db_connect) {
-        ngx_connection_t *c = pdc->connection;
         if (c->read->timer_set) ngx_del_timer(c->read);
         if (c->write->timer_set) ngx_del_timer(c->write);
     }
