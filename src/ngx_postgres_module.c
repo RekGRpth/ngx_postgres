31d2a24f810b9b65b73be4b1239dbc4cc7b9f159
RekGRpth <rekgrpth@gmail.com>
2020-03-24 10:56:51 +0500

    up

--- src/ngx_postgres_module.c
+++ src/ngx_postgres_module.c
@@ -431,7 +431,7 @@ static char *ngx_postgres_server_conf(ngx_conf_t *cf, ngx_command_t *cmd, void *
     ngx_pfree(cf->pool, conninfo.data);
     arg = 0;
     for (const char **keywords = connect->keywords, **values = connect->values; *keywords; keywords++, values++, arg++) {
-        ngx_log_error(NGX_LOG_EMERG, cf->log, 0, "%i: %s = %s", arg, *keywords, *values ? *values : "(null)");
+        ngx_log_error(NGX_LOG_EMERG, cf->log, 0, "%i: %s = %s in upstream \"%V\" in %s:%ui", arg, *keywords, *values ? *values : "(null)", &usc->host, usc->file_name, usc->line);
     }
 //    usc->flags = NGX_HTTP_UPSTREAM_CREATE|NGX_HTTP_UPSTREAM_WEIGHT|NGX_HTTP_UPSTREAM_MAX_CONNS|NGX_HTTP_UPSTREAM_MAX_FAILS|NGX_HTTP_UPSTREAM_FAIL_TIMEOUT|NGX_HTTP_UPSTREAM_DOWN|NGX_HTTP_UPSTREAM_BACKUP;
 //    ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "line = %i", usc->line);
