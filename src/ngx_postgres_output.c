bb5ff6d56662662dd9851ba196db629443762203
RekGRpth <rekgrpth@gmail.com>
2020-08-22 09:36:26 +0500

    up

--- src/ngx_postgres_output.c
+++ src/ngx_postgres_output.c
@@ -486,11 +486,22 @@ ngx_int_t ngx_postgres_output_chain(ngx_postgres_data_t *pd) {
         if (pdc->charset.len) r->headers_out.charset = pdc->charset;
         ngx_http_clear_content_length(r);
         r->headers_out.content_length_n = 0;
-        for (ngx_chain_t *chain = u->out_bufs; chain; chain = chain->next) r->headers_out.content_length_n += chain->buf->end - chain->buf->start;
+        for (ngx_chain_t *chain = u->out_bufs; chain; chain = chain->next) {
+            r->headers_out.content_length_n += chain->buf->end - chain->buf->start;
+            if (!chain->next) {
+                if (r == r->main && !r->post_action) {
+                    chain->buf->last_buf = 1;
+                } else {
+                    chain->buf->sync = 1;
+                    chain->buf->last_in_chain = 1;
+                }
+            }
+        }
         ngx_int_t rc = ngx_http_send_header(r);
         if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) return rc;
     }
     u->header_sent = 1;
+    if (!u->out_bufs) return NGX_OK;
     ngx_int_t rc = ngx_http_output_filter(r, u->out_bufs);
     if (rc != NGX_OK) return rc;
     ngx_chain_update_chains(r->pool, &u->free_bufs, &u->busy_bufs, &u->out_bufs, u->output.tag);
