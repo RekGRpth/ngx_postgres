f14955ddc25798b97aa85606ad7891fcfd74347e
RekGRpth <rekgrpth@gmail.com>
2021-05-13 09:43:49 +0500

    op

--- src/ngx_postgres_upstream.c
+++ src/ngx_postgres_upstream.c
@@ -384,9 +384,8 @@ exit:
     if (!(addr.len = ngx_sock_ntop(pc->sockaddr, pc->socklen, addr.data, NGX_SOCKADDR_STRLEN, 0))) { ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "!ngx_sock_ntop"); return NGX_ERROR; }  // ngx_http_upstream_finalize_request(r, u, NGX_HTTP_INTERNAL_SERVER_ERROR) and return
     const char *host = connect->values[0];
     connect->values[0] = (const char *)addr.data + (pc->sockaddr->sa_family == AF_UNIX ? 5 : 0);
-    int arg = 0;
-    for (const char **keywords = connect->keywords, **values = connect->values; *keywords; keywords++, values++, arg++) {
-        ngx_log_debug3(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "%i: %s = %s", arg, *keywords, *values);
+    for (int i = 0; connect->keywords[i]; i++) {
+        ngx_log_debug3(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "%i: %s = %s", i, connect->keywords[i], connect->values[i]);
     }
     pdc->conn = PQconnectStartParams(connect->keywords, connect->values, 0);
     connect->values[0] = host;
