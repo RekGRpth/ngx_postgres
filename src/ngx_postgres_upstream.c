853f2adf3b86e28954bf52250f5a2d38262f1964
RekGRpth <rekgrpth@gmail.com>
2021-05-14 10:10:46 +0500

    op

--- src/ngx_postgres_upstream.c
+++ src/ngx_postgres_upstream.c
@@ -3,9 +3,9 @@
 #include "ngx_postgres_include.h"
 
 
-static void ngx_postgres_save_to_data(ngx_postgres_save_t *ps, ngx_postgres_data_t *pd) {
+static void ngx_postgres_save_to_data(ngx_log_t *log, ngx_postgres_save_t *ps, ngx_postgres_data_t *pd) {
     ngx_http_request_t *r = pd->request;
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "%s", __func__);
+    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, log, 0, "%s", __func__);
     ngx_http_upstream_t *u = r->upstream;
     ngx_peer_connection_t *pc = &u->peer;
     ngx_postgres_common_t *pdc = &pd->common;
@@ -15,13 +15,11 @@ static void ngx_postgres_save_to_data(ngx_postgres_save_t *ps, ngx_postgres_data
     c->data = r;
     c->idle = 0;
     c->log_error = pc->log_error;
-    c->log = pc->log;
-    c->pool->log = pc->log;
-    c->read->log = pc->log;
-    if (c->read->timer_set) ngx_del_timer(c->read);
+    c->log = log;
+    c->pool->log = log;
+    c->read->log = log;
     c->sent = 0;
-    c->write->log = pc->log;
-    if (c->write->timer_set) ngx_del_timer(c->write);
+    c->write->log = log;
     pc->cached = 1;
     pc->name = &pdc->addr.name;
     pc->sockaddr = pdc->addr.sockaddr;
@@ -42,7 +40,10 @@ static ngx_int_t ngx_postgres_peer_multi(ngx_http_request_t *r) {
         if (ngx_memn2cmp((u_char *)pdc->addr.sockaddr, (u_char *)psc->addr.sockaddr, pdc->addr.socklen, psc->addr.socklen)) continue;
         ngx_queue_remove(&ps->queue);
         ngx_queue_insert_tail(&pusc->ps.data.queue, &ps->queue);
-        ngx_postgres_save_to_data(ps, pd);
+        ngx_postgres_save_to_data(r->connection->log, ps, pd);
+        ngx_connection_t *c = pdc->connection;
+        if (c->read->timer_set) ngx_del_timer(c->read);
+        if (c->write->timer_set) ngx_del_timer(c->write);
         return NGX_OK;
     }
     return NGX_DECLINED;
@@ -211,31 +212,28 @@ close:
 }
 
 
-static void ngx_postgres_data_to_save(ngx_postgres_data_t *pd, ngx_postgres_save_t *ps) {
+static void ngx_postgres_data_to_save(ngx_log_t *log, ngx_postgres_data_t *pd, ngx_postgres_save_t *ps) {
     ngx_http_request_t *r = pd->request;
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "%s", __func__);
+    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, log, 0, "%s", __func__);
     ngx_http_upstream_t *u = r->upstream;
     ngx_peer_connection_t *pc = &u->peer;
     pc->connection = NULL;
     ngx_postgres_common_t *psc = &ps->common;
     ngx_postgres_common_t *pdc = &pd->common;
-    ngx_postgres_upstream_srv_conf_t *pusc = pdc->pusc;
     *psc = *pdc;
     ngx_connection_t *c = psc->connection;
     c->data = ps;
     c->idle = 1;
-    c->log = pusc->ps.save.log ? pusc->ps.save.log : ngx_cycle->log;
+    c->log = log;
     c->log->connection = c->number;
-    c->pool->log = c->log;
+    c->pool->log = log;
 //    c->read->delayed = 0;
     c->read->handler = ngx_postgres_save_handler;
-    c->read->log = c->log;
-    ngx_add_timer(c->read, pusc->ps.save.timeout);
+    c->read->log = log;
     c->read->timedout = 0;
 //    c->write->delayed = 0;
     c->write->handler = ngx_postgres_save_handler;
-    c->write->log = c->log;
-    ngx_add_timer(c->write, pusc->ps.save.timeout);
+    c->write->log = log;
     c->write->timedout = 0;
 }
 
@@ -275,7 +273,9 @@ static void ngx_postgres_free_peer(ngx_http_request_t *r) {
     }
     ngx_queue_remove(&ps->queue);
     ngx_queue_insert_tail(&pusc->ps.save.queue, &ps->queue);
-    ngx_postgres_data_to_save(pd, ps);
+    ngx_postgres_data_to_save(pusc->ps.save.log ? pusc->ps.save.log : ngx_cycle->log, pd, ps);
+    ngx_add_timer(c->read, pusc->ps.save.timeout);
+    ngx_add_timer(c->write, pusc->ps.save.timeout);
     if (listen) {
         ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "listen = %s", listen);
         if (!PQsendQuery(pdc->conn, (const char *)listen)) { ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "!PQsendQuery(\"%s\") and %s", listen, PQerrorMessageMy(pdc->conn)); }
