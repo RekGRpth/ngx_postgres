dc0b0643b1b1a9133ee66a98021b4677fc33b02b
RekGRpth <rekgrpth@gmail.com>
2021-06-02 10:06:51 +0500

    up

--- src/ngx_postgres_module.c
+++ src/ngx_postgres_module.c
@@ -320,7 +320,8 @@ static ngx_int_t ngx_postgres_connect_conf(ngx_conf_t *cf, ngx_command_t *cmd, n
 static char *ngx_postgres_server_conf(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
     ngx_http_upstream_srv_conf_t *usc = ngx_http_conf_get_module_srv_conf(cf, ngx_http_upstream_module);
     ngx_postgres_upstream_srv_conf_t *pusc = conf;
-    if (usc->peer.init_upstream != ngx_postgres_peer_init_upstream) { pusc->peer.init_upstream = usc->peer.init_upstream; usc->peer.init_upstream = ngx_postgres_peer_init_upstream; }
+    pusc->peer.init_upstream = usc->peer.init_upstream;
+    usc->peer.init_upstream = ngx_postgres_peer_init_upstream;
     if (!usc->servers && !(usc->servers = ngx_array_create(cf->pool, 1, sizeof(*usc->servers)))) { ngx_log_error(NGX_LOG_EMERG, cf->log, 0, "\"%V\" directive error: !ngx_array_create", &cmd->name); return NGX_CONF_ERROR; }
     ngx_http_upstream_server_t *us = ngx_array_push(usc->servers);
     if (!us) { ngx_log_error(NGX_LOG_EMERG, cf->log, 0, "\"%V\" directive error: !ngx_array_push", &cmd->name); return NGX_CONF_ERROR; }
