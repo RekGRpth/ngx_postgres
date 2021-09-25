2fe81d50f6c45972dd6a65d88778344d7045f3a8
RekGRpth <rekgrpth@gmail.com>
2021-09-25 13:13:28 +0500

    fix

--- src/ngx_postgres_handler.c
+++ src/ngx_postgres_handler.c
@@ -72,7 +72,7 @@ void ngx_postgres_data_handler(ngx_event_t *e) {
         if (rc == NGX_OK) rc = ngx_postgres_result(s);
     }
     if (rc == NGX_ERROR) ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_ERROR);
-    else if (rc != NGX_AGAIN) ngx_http_upstream_finalize_request(r, u, rc == NGX_OK ? NGX_DONE : rc);
+    else if (rc != NGX_AGAIN) ngx_http_upstream_finalize_request(r, u, rc == NGX_OK ? NGX_HTTP_OK : rc);
 run:
     ngx_http_run_posted_requests(co);
 }
@@ -137,7 +137,7 @@ static void ngx_postgres_finalize_request(ngx_http_request_t *r, ngx_int_t rc) {
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "rc = %i", rc);
     ngx_http_upstream_t *u = r->upstream;
     if (u->peer.get != ngx_postgres_peer_get) { ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "peer is not postgres"); return; }
-    if (rc == NGX_DONE) ngx_postgres_output(r);
+    if (rc == NGX_HTTP_OK) ngx_postgres_output(r);
     ngx_postgres_data_t *d = u->peer.data;
     ngx_postgres_save_t *s = d->save;
     if (!s) return;
