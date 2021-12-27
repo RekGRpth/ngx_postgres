63557a578408b7f79375fa2f9b02875a503c5214
RekGRpth <rekgrpth@gmail.com>
2021-12-27 13:43:55 +0500

    op

--- src/ngx_postgres_handler.c
+++ src/ngx_postgres_handler.c
@@ -20,8 +20,11 @@ void ngx_postgres_data_read_handler(ngx_event_t *e) {
     }
     s->res = NULL;
     if (rc == NGX_OK) rc = s->handler(s);
-    if (rc == NGX_ERROR) ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_ERROR);
-    else if (rc != NGX_AGAIN) ngx_http_upstream_finalize_request(r, u, rc == NGX_OK && u->out_bufs ? NGX_HTTP_OK : rc);
+    switch (rc) {
+        case NGX_ERROR: ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_ERROR); break;
+        case NGX_AGAIN: break;
+        default: ngx_http_upstream_finalize_request(r, u, rc == NGX_OK && u->out_bufs ? NGX_HTTP_OK : rc);
+    }
 run:
     ngx_http_run_posted_requests(co);
 }
@@ -39,15 +42,18 @@ void ngx_postgres_data_write_handler(ngx_event_t *e) {
     if (c->write->timedout) { c->write->timedout = 0; PQstatus(s->conn) == CONNECTION_OK ? ngx_http_upstream_finalize_request(r, u, NGX_HTTP_GATEWAY_TIME_OUT) : ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_TIMEOUT); goto run; }
     if (ngx_http_upstream_test_connect(c) != NGX_OK) { ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_ERROR); goto run; }
     ngx_int_t rc = NGX_OK;
-    if (PQstatus(s->conn) == CONNECTION_OK && rc == NGX_OK) rc = ngx_postgres_notify(s);
+//    if (PQstatus(s->conn) == CONNECTION_OK && rc == NGX_OK) rc = ngx_postgres_notify(s);
     while (PQstatus(s->conn) == CONNECTION_OK && (s->res = PQgetResult(s->conn))) {
         if (rc == NGX_OK) rc = s->handler(s);
         PQclear(s->res);
     }
     s->res = NULL;
     if (rc == NGX_OK) rc = s->handler(s);
-    if (rc == NGX_ERROR) ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_ERROR);
-    else if (rc != NGX_AGAIN) ngx_http_upstream_finalize_request(r, u, rc == NGX_OK && u->out_bufs ? NGX_HTTP_OK : rc);
+    switch (rc) {
+        case NGX_ERROR: ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_ERROR); break;
+        case NGX_AGAIN: break;
+        default: ngx_http_upstream_finalize_request(r, u, rc == NGX_OK && u->out_bufs ? NGX_HTTP_OK : rc);
+    }
 run:
     ngx_http_run_posted_requests(co);
 }
